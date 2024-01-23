9e86749aba606632c03d39d07881498adceba719
cakirburak <cakirburak43@hotmail.com>
2024-01-23 15:09:09 +0300

    about page implemented

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,21 +15,21 @@ import Search from './pages/Search';
 import { Footer } from './components/Footer';
 
 export default function App() {
-  return <BrowserRouter>
-    <Header />
-    <Routes>
-      <Route path='/' element={<Home />}></Route>
-      <Route path='/sign-in' element={<SignIn />}></Route>
-      <Route path='/sign-up' element={<SignUp />}></Route>
-      <Route path='/about' element={<About />}></Route>
-      <Route path='/search' element={<Search />}></Route>
-      <Route element={<PrivateProfileRoute />}>
-        <Route path='/profile' element={<Profile />}></Route>
-        <Route path='/create-listing' element={<CreateListing />}></Route>
-        <Route path='/update-listing/:listingId' element={<UpdateListing />}></Route>
-      </Route>
-      <Route path='/listing/:listingId' element={<Listing />}></Route>
-    </Routes>
-    <Footer />
-  </BrowserRouter>;
+	return <BrowserRouter>
+		<Header />
+		<Routes>
+			<Route path='/' element={<Home />}></Route>
+			<Route path='/sign-in' element={<SignIn />}></Route>
+			<Route path='/sign-up' element={<SignUp />}></Route>
+			<Route path='/about' element={<About />}></Route>
+			<Route path='/search' element={<Search />}></Route>
+			<Route element={<PrivateProfileRoute />}>
+				<Route path='/profile' element={<Profile />}></Route>
+				<Route path='/create-listing' element={<CreateListing />}></Route>
+				<Route path='/update-listing/:listingId' element={<UpdateListing />}></Route>
+			</Route>
+			<Route path='/listing/:listingId' element={<Listing />}></Route>
+		</Routes>
+		<Footer />
+	</BrowserRouter>;
 }
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,35 @@
 import React from 'react'
 
 export default function About() {
-  return (
-    <div>About</div>
-  )
+	return (
+		<div className='mx-4 sm:mx-24'>
+			<h1 className='ms-4 sm:ms-24  sm:text-2xl mt-4 sm:mt-12 font-semibold text-slate-700'>About TEstate</h1>
+			<p className='ms-4 sm:ms-24 mt-4 mb-12'>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, atque. Ex obcaecati neque deleniti officiis autem quam
+				aut nesciunt. Eum repudiandae minima similique nemo sit sapiente molestiae, distinctio animi earum!
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, atque. Ex obcaecati neque deleniti officiis autem quam
+				aut nesciunt. Eum repudiandae minima similique nemo sit sapiente molestiae, distinctio animi earum!
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, atque. Ex obcaecati neque deleniti officiis autem quam
+				aut nesciunt. Eum repudiandae minima similique nemo sit sapiente molestiae, distinctio animi earum!
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, atque. Ex obcaecati neque deleniti officiis autem quam
+				aut nesciunt. Eum repudiandae minima similique nemo sit sapiente molestiae, distinctio animi earum!
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, atque. Ex obcaecati neque deleniti officiis autem quam
+				aut nesciunt. Eum repudiandae minima similique nemo sit sapiente molestiae, distinctio animi earum!
+				<hr className='my-4' />
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, atque. Ex obcaecati neque deleniti officiis autem quam
+				aut nesciunt. Eum repudiandae minima similique nemo sit sapiente molestiae, distinctio animi earum!
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, atque. Ex obcaecati neque deleniti officiis autem quam
+				aut nesciunt. Eum repudiandae minima similique nemo sit sapiente molestiae, distinctio animi earum!
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, atque. Ex obcaecati neque deleniti officiis autem quam
+				aut nesciunt. Eum repudiandae minima similique nemo sit sapiente molestiae, distinctio animi earum!
+				<hr className='my-4' />
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, atque. Ex obcaecati neque deleniti officiis autem quam
+				aut nesciunt. Eum repudiandae minima similique nemo sit sapiente molestiae, distinctio animi earum!
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, atque. Ex obcaecati neque deleniti officiis autem quam
+				aut nesciunt. Eum repudiandae minima similique nemo sit sapiente molestiae, distinctio animi earum!
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, atque. Ex obcaecati neque deleniti officiis autem quam
+				aut nesciunt. Eum repudiandae minima similique nemo sit sapiente molestiae, distinctio animi earum!
+			</p>
+		</div>
+	)
 }
