87b43ac872c110a57474aa66687d941209af8c58
cakirburak <cakirburak43@hotmail.com>
2024-01-23 15:35:55 +0300

    build configuration for deployment of v1.0.0

--- api/index.js
+++ api/index.js
@@ -5,7 +5,10 @@ import authRouter from './routes/auth.route.js'
 import listingRouter from './routes/listing.route.js'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
- 
+import path from 'path'
+
+const __dirname = path.resolve()
+
 dotenv.config()
 
 const port = 3000;
@@ -14,27 +17,33 @@ app.use(express.json())
 app.use(cookieParser())
 
 const runMongoDBConnection = async () => {
-    await mongoose.connect(process.env.MONGO)
-    .then(() => {
-        console.log("Connected to MongoDB")
-    })
+	await mongoose.connect(process.env.MONGO)
+		.then(() => {
+			console.log("Connected to MongoDB")
+		})
 }
 runMongoDBConnection().catch((err) => console.error(err))
 
 app.listen(3000, () => {
-    console.log(`Server is running on port ${port}`)
+	console.log(`Server is running on port ${port}`)
 })
 
 app.use('/api/user', userRouter)
 app.use('/api/auth', authRouter)
 app.use('/api/listing', listingRouter)
 
+app.use(express.static(path.join(__dirname, '/client/dist')))
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
+
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500
-    const message = err.message || 'Internal Server Error'
-    return res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message,
-    })
+	const statusCode = err.statusCode || 500
+	const message = err.message || 'Internal Server Error'
+	return res.status(statusCode).json({
+		success: false,
+		statusCode,
+		message,
+	})
 })
